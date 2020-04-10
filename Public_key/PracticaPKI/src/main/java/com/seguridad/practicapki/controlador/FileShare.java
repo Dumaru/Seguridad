3d3847e4a9c44f06d5177c3e01febba77e3db198
Dumaru <robinsonmu@unisabana.edu.co>
2020-04-10 17:00:18 -0500

    Lectura de claves publicas y privadas, con parseo a objeto key

--- Public_key/PracticaPKI/src/main/java/com/seguridad/practicapki/controlador/FileShare.java
+++ Public_key/PracticaPKI/src/main/java/com/seguridad/practicapki/controlador/FileShare.java
@@ -7,6 +7,7 @@ package com.seguridad.practicapki.controlador;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
@@ -22,13 +23,22 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
 import org.bouncycastle.asn1.ASN1Primitive;
 import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
 import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
 import org.bouncycastle.openssl.PEMKeyPair;
 import org.bouncycastle.openssl.PEMParser;
+import org.bouncycastle.openssl.PKCS8Generator;
+import org.bouncycastle.openssl.jcajce.JcaMiscPEMGenerator;
 import org.bouncycastle.openssl.jcajce.JcaPEMWriter;
+import org.bouncycastle.openssl.jcajce.JcaPKCS8Generator;
+import org.bouncycastle.openssl.jcajce.JceOpenSSLPKCS8EncryptorBuilder;
+import org.bouncycastle.operator.OperatorCreationException;
+import org.bouncycastle.operator.OutputEncryptor;
 import org.bouncycastle.util.io.pem.PemObject;
+import org.bouncycastle.util.io.pem.PemReader;
 
 /**
  *
@@ -40,7 +50,9 @@ public class FileShare {
     public static final int BIT_LENGTH = 1024;
 
     public FileShare() {
-
+        java.security.Security.addProvider(
+                new org.bouncycastle.jce.provider.BouncyCastleProvider()
+        );
     }
 
     /**
@@ -52,7 +64,7 @@ public class FileShare {
      * @throws InvalidKeySpecException
      * @throws KeyStoreException
      */
-    public String[] crearClaves(String pathBaseClaves, String nombrePriv, String nombrePub) throws NoSuchAlgorithmException, InvalidKeySpecException, KeyStoreException, IOException {
+    public String[] crearClaves(String pathBaseClaves, String nombrePriv, String nombrePub) throws NoSuchAlgorithmException, InvalidKeySpecException, KeyStoreException, IOException, OperatorCreationException {
         // Genera las claves privada y publica
         KeyPairGenerator generadorClaves = KeyPairGenerator.getInstance("RSA");
         generadorClaves.initialize(BIT_LENGTH);
@@ -82,15 +94,20 @@ public class FileShare {
      * @param pathBase
      * @throws IOException
      */
-    private void guardarClaves(KeyPair parClaves, String nombrePriv, String nombrePub) throws IOException {
+    private void guardarClaves(KeyPair parClaves, String nombrePriv, String nombrePub) throws IOException, OperatorCreationException {
         // Crea writter de clave privada 
         // TODO: CODIFICAR CLAVES:
-        FileWriter archivoClavePriv = new FileWriter(Path.of(nombrePriv).toFile());
+        FileWriter archivoClavePriv = new FileWriter(Path.of(nombrePriv + "_privada.pem").toFile());
         JcaPEMWriter pemWriter = new JcaPEMWriter(archivoClavePriv);
+        // Private key in PKCS8 
+        //JcaMiscPEMGenerator generator = new JcaMiscPEMGenerator(parClaves.getPrivate());
+        //pemWriter.writeObject(generator.generate());
+
         pemWriter.writeObject(parClaves.getPrivate());
+
         pemWriter.close();
         // Crea writter de clave publica
-        FileWriter archivoClavePub = new FileWriter(Path.of(nombrePub + ".pub").toFile());
+        FileWriter archivoClavePub = new FileWriter(Path.of(nombrePub + "_publica.pem").toFile());
         pemWriter = new JcaPEMWriter(archivoClavePub);
         pemWriter.writeObject(parClaves.getPublic());
         pemWriter.close();
@@ -98,40 +115,51 @@ public class FileShare {
 
     /**
      * Lee un archivo y devuelve el arreglo de bytes de la clave codificada
+     *
      * @param pathClave
      * @return
      * @throws FileNotFoundException
-     * @throws IOException 
+     * @throws IOException
      */
-    public byte[] leerClave(String pathClave) throws FileNotFoundException, IOException {
-        byte[] clave = null;
-        InputStreamReader archivo = new InputStreamReader(new FileInputStream(pathClave));
-        PEMParser parser = new PEMParser(archivo);
-        Object objeto = parser.readObject();
-        System.out.println("Objeto " + objeto.toString());
-        if (objeto instanceof SubjectPublicKeyInfo) {
-            System.out.println("Clave publica ");
-            SubjectPublicKeyInfo keyInfo = ((SubjectPublicKeyInfo) objeto);
-            ASN1Primitive primitive = keyInfo.parsePublicKey();
-            System.out.println("Info public key " + primitive.toString());
-            System.out.println("Public key codificada " + Arrays.toString(primitive.getEncoded()));
-            clave = primitive.getEncoded();
-        } else {
-            System.out.println("Clave privada ");
-            PEMKeyPair pair = (PEMKeyPair) objeto;
-            PrivateKeyInfo infoPrivada =  pair.getPrivateKeyInfo();
-            ASN1Primitive primitive =  infoPrivada.parsePrivateKey().toASN1Primitive();
-            System.out.println("Info privada "+primitive.toString());
-            System.out.println("Key Privada codificada"+Arrays.toString(primitive.getEncoded()));
-            clave = primitive.getEncoded();
+    public Key leerClave(String pathClave) throws FileNotFoundException, IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+        Key clave = null;
+        FileInputStream archivo = new FileInputStream(pathClave);
+        PemReader pemReader = new PemReader(new InputStreamReader(archivo));
+        PemObject objetoPem = pemReader.readPemObject();
+        byte[] pemContent = objetoPem.getContent();
+        pemReader.close();
+        System.out.println("Objeto pem " + objetoPem.getType() + " " + "\nContenido objeto pem " + Arrays.toString(pemContent));
+        KeyFactory factory = KeyFactory.getInstance("RSA");
+        if (objetoPem.getType().contains("PRIVATE KEY")) {
+            PKCS8EncodedKeySpec encodedKeySpec = new PKCS8EncodedKeySpec(pemContent);
+            clave = factory.generatePrivate(encodedKeySpec);
+            System.out.println("Clave privada " + Arrays.toString(clave.getEncoded()));
+        } else if (objetoPem.getType().contains("PUBLIC KEY")) {
+            EncodedKeySpec encodedKeySpec = new X509EncodedKeySpec(pemContent);
+            clave = factory.generatePublic(encodedKeySpec);
+            System.out.println("Clave publica " + Arrays.toString(clave.getEncoded()));
         }
         return clave;
     }
 
-    public String cifrarArchivo(String pathArchivoCifrar, String pathClavePublica) {
-        // Cifra el archivo y retorna el direccion donde esta el archivo
-        String pathCifrado = "";
+    /**
+     * Lee un archivo y retorna la secuencia de bytes de este
+     *
+     * @param pathArchivo
+     * @return
+     * @throws FileNotFoundException
+     * @throws IOException
+     */
+    private byte[] leerArchivo(String pathArchivo) throws FileNotFoundException, IOException {
+        byte[] archivo = null;
+        FileInputStream file = new FileInputStream(pathArchivo);
+        archivo = file.readAllBytes();
+        return archivo;
+    }
 
+    public String cifrarArchivo(String pathCifrado, String pathArchivoCifrar, String pathClavePublica) throws NoSuchAlgorithmException, NoSuchPaddingException {
+        // Cifra el archivo y retorna el direccion donde esta el archivo
+        Cipher cifradorRsa = Cipher.getInstance("RSA");
         return pathCifrado;
     }
 
@@ -176,10 +204,27 @@ public class FileShare {
     }
 
     public static void main(String[] args) {
+
         FileShare fileShare = new FileShare();
         System.out.println(FileShare.gcd(BigInteger.valueOf(7), BigInteger.valueOf(120)));
-        /*        try {
-            String[] parClaves = fileShare.crearClaves("C:\\claves", "privada", "publica");
+
+        try {
+            String[] parClaves = fileShare.crearClaves("C:\\claves", "miclave", "miclave");
+
+            try {
+                //Key claveRsa = fileShare.leerClave("C:\\claves\\publica.pub");
+                Key claveRsa = fileShare.leerClave("C:\\claves\\miclave_publica.pem");
+                if (claveRsa instanceof PrivateKey) {
+                    PrivateKey claveRsaPrivada = (PrivateKey) claveRsa;
+                    System.out.println("Clave rsa privada: " + claveRsaPrivada);
+                }else{
+                    PublicKey claveRsaPublica = (PublicKey) claveRsa;
+                    System.out.println("Clave rsa publica: " + claveRsaPublica);
+                }
+            } catch (IOException ex) {
+                Logger.getLogger(FileShare.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
         } catch (NoSuchAlgorithmException ex) {
             Logger.getLogger(FileShare.class.getName()).log(Level.SEVERE, null, ex);
         } catch (InvalidKeySpecException ex) {
@@ -188,13 +233,7 @@ public class FileShare {
             Logger.getLogger(FileShare.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
             Logger.getLogger(FileShare.class.getName()).log(Level.SEVERE, null, ex);
-        }
-         */
-        try {
-            //Key claveRsa = fileShare.leerClave("C:\\claves\\publica.pub");
-            byte[] claveRsa = fileShare.leerClave("C:\\claves\\privada");
-            System.out.println("La Clave "+ Arrays.toString(claveRsa));
-        } catch (IOException ex) {
+        } catch (OperatorCreationException ex) {
             Logger.getLogger(FileShare.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
